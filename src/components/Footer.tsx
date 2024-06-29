d74c9c58fe693417e680db962599421e78efcb89
engrkashem <kashemaust@gmail.com>
2024-06-29 17:24:14 +0600

    draft footer design added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import footerStyle from "../styles/footer.module.css";
 export default function Footer() {
   return (
-    <footer>
-      <h1>This is Footer page</h1>
+    <footer className={footerStyle.container}>
+      <i className={`fa-solid fa-blog ${footerStyle.logo}`}></i>
+      <div className={footerStyle.content}>
+        <div className={footerStyle.navContainer}>
+          <Link to={"/"}>
+            <i className={`fa-solid fa-house ${footerStyle.navIcon}`}></i>
+          </Link>
+          <Link to={"/article"}>
+            <i className={`fa-solid fa-newspaper ${footerStyle.navIcon}`}></i>
+          </Link>
+        </div>
+        <small className={footerStyle.text}>
+          All right reserved to @kashem:{new Date().getFullYear()}
+        </small>
+      </div>
     </footer>
   );
 }
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import layoutStyle from "../styles/mainLayout.module.css";
 
 export default function MainLayout() {
   return (
-    <div>
+    <div className={layoutStyle.container}>
       <NavBar />
-      <div>
+      <div className={layoutStyle.contentContainer}>
         <Outlet />
       </div>
       <Footer />
