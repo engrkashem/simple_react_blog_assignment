4698813d9ee7aa3cccff18fcce8006c2a10ac3c6
engrkashem <kashemaust@gmail.com>
2024-06-30 16:55:40 +0600

    search box functionality added

--- src/components/Home/Search.tsx
+++ src/components/Home/Search.tsx
@@ -1,19 +1,43 @@
+import { useContext, useState } from "react";
+import StoreContext from "../../context/storeContext";
 import searchStyle from "../../styles/Home/search.module.css";
 
 export default function Search() {
+  // local state
+  const [searchText, setSearchText] = useState("");
+
+  // context set state function by useContext hooks
+  const { setCategory } = useContext(StoreContext);
+
+  // search box submit handler
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // check if setCategory function available in the context store
+    if (setCategory) {
+      setCategory(searchText);
+    }
+
+    // setting search text empty to reset search box after submit
+    setSearchText("");
+  };
+
   return (
-    <div className={searchStyle.container}>
+    <form className={searchStyle.container} onSubmit={handleSearch}>
+      {/* search input field  */}
       <input
+        onChange={(e) => setSearchText(e.target.value)}
         className={searchStyle.searchField}
         type="text"
         name="search"
         id="search"
         placeholder="Search"
+        value={searchText}
       />
-
+      {/* search icon  */}
       <i
         className={`fa-solid fa-magnifying-glass ${searchStyle.searchIcon}`}
       ></i>
-    </div>
+    </form>
   );
 }
