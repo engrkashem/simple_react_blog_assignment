ac2473590d05da41d774f2219ed468b6f01cddc6
engrkashem <kashemaust@gmail.com>
2024-07-03 15:13:05 +0600

    home banner responsive made

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,17 @@ import homeStyle from "../styles/home.module.css";
 export default function Home() {
   return (
     <div className={homeStyle.container}>
-      <h1 className={homeStyle.title}>
-        <span className="twoEm">LEARN </span>First
-        <br />
-        Then{" "}
-        <span className="twoEm ">
-          <span className="line-through colorGray">L</span>EARN
-        </span>
-      </h1>
+      <div className={`${homeStyle.half} ${homeStyle.left}`}></div>
+      <div className={`${homeStyle.half} ${homeStyle.right}`}>
+        <h1 className={homeStyle.title}>
+          <span className="twoEm">LEARN </span>First
+          <br />
+          Then{" "}
+          <span className="twoEm ">
+            <span className="line-through colorGray">L</span>EARN
+          </span>
+        </h1>
+      </div>
     </div>
   );
 }
